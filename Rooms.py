f24285c5aca09d81c86132e7f91b389f92d0f6c1
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-15 23:45:44 +0300

    Добавил проверку существует ли таблица и ее создание, если она не существует

--- Rooms.py
+++ Rooms.py
@@ -11,6 +11,8 @@ class RoomDB:
     def __init__(self, mysql_connector, data_mysql):
         self.MysqlConnector = mysql_connector
         self.data_mysql = data_mysql
+        if not self.check_table():
+            self.create_table()
 
     def load_in_db(self, rooms: list) -> None:
         pass
@@ -33,3 +35,11 @@ class RoomDB:
                   'id INTEGER PRIMARY KEY NOT NULL, ' \
                   'name VARCHAR(100) NOT NULL)'
             db.execute(sql)
+
+    def check_table(self):
+        with self.MysqlConnector(self.data_mysql) as db:
+            sql = 'show tables like \'rooms\''
+            db.execute(sql)
+            response = db.fetchone()
+            table_is_created = response is not None and "rooms" in response
+        return table_is_created
--- Students.py
+++ Students.py
@@ -4,16 +4,38 @@ from dataclasses import dataclass
 
 @dataclass
 class Student:
-    birthday: str
     id: int
     name: str
+    birthday: str
     room: int
     sex: str
 
 
 class StudentDB:
-    def __init__(self, mysql_connector):
-        self.mysql_connector = mysql_connector
+    def __init__(self, mysql_connector, data_mysql):
+        self.MysqlConnector = mysql_connector
+        self.data_mysql = data_mysql
+        if not self.check_table():
+            self.create_table()
 
     def load_in_db(self, students: list) -> None:
         pass
+
+    def create_table(self):
+        with self.MysqlConnector(self.data_mysql) as db:
+            sql = 'CREATE TABLE students(' \
+                  'id INTEGER PRIMARY KEY NOT NULL, ' \
+                  'birthday DATETIME,' \
+                  'name VARCHAR(100) NOT NULL,' \
+                  'sex VARCHAR(1),' \
+                  'room INTEGER,' \
+                  'FOREIGN KEY (room) REFERENCES rooms(id))'
+            db.execute(sql)
+
+    def check_table(self):
+        with self.MysqlConnector(self.data_mysql) as db:
+            sql = 'show tables like \'students\''
+            db.execute(sql)
+            response = db.fetchone()
+            table_is_created = response is not None and "students" in response
+        return table_is_created
