e04124887660ca5b2e93342ed47c141436b52457
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-15 23:44:44 +0300

    Добавил конвертацию данных из json в информацию типа student и room

--- FileReader.py
+++ FileReader.py
@@ -1,8 +1,10 @@
 import json
+from Rooms import Room
+from Students import Student
 
 
-class FileReader():
-    def load_json_file(self, path: str):
+class JSONReader:
+    def _load_json_file(self, path: str) -> list:
         try:
             with open(path, 'r', encoding='utf-8') as file:
                 json_text = json.load(file)
@@ -11,3 +13,18 @@ class FileReader():
         except json.decoder.JSONDecodeError:
             raise Exception("Неверный формат json")
         return json_text
+
+    def get_rooms(self, path: str) -> list:
+        json_list = self._load_json_file(path)
+        rooms = []
+        for json_room in json_list:
+            rooms.append(Room(json_room["id"], json_room["name"]))
+        return rooms
+
+    def get_students(self, path: str) -> list:
+        json_list = self._load_json_file(path)
+        students = []
+        for json_student in json_list:
+            students.append(Student(json_student["id"], json_student["name"], json_student["birthday"],
+                                    json_student["room"], json_student["sex"]))
+        return students
