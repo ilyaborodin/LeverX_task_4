a2aedfe2e88345bc2d00a28383619b6682987f6c
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-16 14:31:47 +0300

    Добавил код для реализации программы

--- main.py
+++ main.py
@@ -17,8 +17,18 @@ def main():
     room_db.load_in_db(rooms)
     students = json_reader.get_students(path_to_students)
     student_db.load_in_db(students)
+
     all_rooms = room_db.get_all()
-    data_exporter.export_to(all_rooms, format_of_output, "all_rooms_with_counter")
+    data_exporter.export_to(all_rooms, format_of_output, "[1]all_rooms_with_counter")
+
+    avg_rooms = room_db.get_with_smallest_avg_age()
+    data_exporter.export_to(avg_rooms, format_of_output, "[2]rooms_with_smallest_avg_age")
+
+    rooms_with_big_delta = room_db.get_with_the_biggest_difference_in_ages()
+    data_exporter.export_to(rooms_with_big_delta, format_of_output, "[3]rooms_with_the_biggest_difference_in_ages")
+
+    rooms_with_heterosexuals = room_db.get_with_heterosexuals()
+    data_exporter.export_to(rooms_with_heterosexuals, format_of_output, "[4]rooms_with_heterosexuals")
 
 
 if __name__ == '__main__':
